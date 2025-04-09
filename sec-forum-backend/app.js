c6c7fff910ac69a95c6c133b2097ad25dcf90895
Olli Sund <olli.sund@tuni.fi>
2025-04-09 23:08:23 +0300

    Connect backend to mongodb.

--- sec-forum-backend/app.js
+++ sec-forum-backend/app.js
@@ -3,11 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const { version } = require('os');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 var app = express();
+require('dotenv').config();
+
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi:{
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationsErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
